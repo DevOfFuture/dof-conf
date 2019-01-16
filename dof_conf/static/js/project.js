f9f09977da844e91c1e0e60c37392d81c11c5f8e
Valery Melou <valery@valerymelou.com>
2019-01-16 10:42:13 +0100

    feat: implement the hero section of the page

--- dof_conf/static/js/project.js
+++ dof_conf/static/js/project.js
@@ -1,1 +1,30 @@
 /* Project specific Javascript goes here. */
+
+var daysText = $('.value.days');
+var hoursText = $('.value.hours');
+var minutesText = $('.value.minutes');
+var secondsText = $('.value.seconds');
+
+var updateCounter = function() {
+    var eventDate = moment(new Date('2019-02:10 08:00'));
+    var now = moment(new Date());
+    // This is ugly but it will do for now ;-)
+    daysText.text(eventDate.diff(now, 'days'));
+    hoursText.text(eventDate.subtract(eventDate.diff(now, 'days'), 'days').diff(now, 'hours'));
+    minutesText.text(eventDate.subtract(eventDate.diff(now, 'hours'), 'hours').diff(now, 'minutes'));
+    secondsText.text(eventDate.subtract(eventDate.diff(now, 'minutes'), 'minutes').diff(now, 'seconds'));
+}
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+            $('.navbar').removeClass('hidden');
+        } else {
+            $('.navbar').addClass('hidden');
+        }
+    });
+
+    setInterval(function() {
+        updateCounter();
+    }, 1000);
+});
